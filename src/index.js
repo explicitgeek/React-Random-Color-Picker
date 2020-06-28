7e266e6f796a74d3e4800f5f154ddea3b2921143
explicit_geek <48882821+explicitgeek@users.noreply.github.com>
2020-06-29 01:23:09 +0530

    Initial Commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Random from './components/Random'
+
+
+
+ReactDOM.render(
+ 
+    <Random/>,
+document.getElementById('app')
+);
+
+
